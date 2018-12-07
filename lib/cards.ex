4f279e5e7a81a7e7cea082dbff7b4dea1dd299fb
Maria Alonso <maria.alonso@cabify.com>
2018-12-07 09:47:56 +0100

    added testing for create deck and shuffle

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,7 +2,7 @@ defmodule Cards do
 
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
-    suits = ["Spades", "Clubs", "Hearts", "Diamons", "Kings"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamons"]
 
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
